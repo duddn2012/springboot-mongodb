c957dc23cb3bdf2d41306f8c89773441c5674cd9
duddn2012 <gowoo123@naver.com>
2023-09-07 22:52:11 +0900

    interface prac

--- src/main/java/com/gitit/letsgitit/user/UserServiceImpl.java
+++ src/main/java/com/gitit/letsgitit/user/UserServiceImpl.java
@@ -2,7 +2,6 @@ package com.gitit.letsgitit.user;
 
 import com.gitit.letsgitit.user.dto.GetUserReq;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
